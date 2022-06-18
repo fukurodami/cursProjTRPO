33f829cd25207e53550340e91d6c1e3653b57502
fukurodami <fukurodami@gmail.com>
2022-06-18 16:55:50 +0700

    18/06

--- furniture/models.py
+++ furniture/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+class Color(models.Model):
+    """Продукт"""
+    title = models.CharField("Название", max_length=100)
+    photo = models.ImageField("Фото",  upload_to="colors/")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Цвет"
+        verbose_name_plural = "Цвета"
+
+class Group(models.Model):
+    """Группа"""
+    title = models.CharField("Название", max_length=100)
+    photo = models.ImageField("Фото",  upload_to="products/")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
+
+class Artikle(models.Model):
+    """Артикл"""
+    title = models.CharField("Название", max_length=100)
+    artikle = models.CharField("Артикул", max_length=30)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Автикл"
+        verbose_name_plural = "Артиклы"
+
+class Product(models.Model):
+    """Продукт"""
+    title = models.CharField("Название", max_length=100)
+    artikle = models.ForeignKey(Artikle, verbose_name="Артикул", on_delete=models.CASCADE)
+    width = models.PositiveIntegerField("Ширина")
+    height = models.PositiveIntegerField("Высота")
+    depth = models.PositiveIntegerField("Глубина")
+    coast = models.PositiveIntegerField("Цена")
+    colors = models.ManyToManyField(Color, verbose_name="Основной цвет")
+    ffrancia = models.PositiveIntegerField("Гарантия")
+    photo = models.ImageField("Фото",  upload_to="products/")
+    group = models.ForeignKey(Group, verbose_name="Группа", on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукт"
+
+
